33d9f4588b3e07eda6a63be77394f333fbc34930
Nikita_Jagdale <nikitajagdale88@email.com>
2023-09-23 21:28:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MyBookStore from './components/MyBookStore';
+import BookList from './components/BookList';
+import Books from './components/Books';
+import BookDetails from './components/BookDetails';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App =()=>{
+
+
+    return(
+        <>
+         <MyBookStore />
+         <BookList />
+         <Books />
+         <BookDetails />
+         <Navbar />
+
+        </>
+    );
 }
-
 export default App;
